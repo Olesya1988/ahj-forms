66e1214849f5bac569b534b0f9b18378edf6ef71
Olesya Vetoshkina <our_post-office@mail.ru>
2023-07-07 04:42:03 +0300

    next commit

--- src/js/Tooltip.js
+++ src/js/Tooltip.js
@@ -1,15 +1,57 @@
-import Draw from './Draw';
-
 export default class Tooltip {
   constructor(container) {
     this.container = container;
-    this.draw = new Draw(this.container);
+    this.title = 'Popover title';
+    this.message = 'And here\'s some amazing content. It\'s very engaging. Right?';
   }
 
   init() {
-    this.draw.drawUI();
-    this.draw.drawTooltip();
+    this.drawUI();
+    this.drawTooltip();
     this.bindToDOM();
+  }
+
+  drawUI() {
+    const form = document.createElement('div');
+    form.innerHTML = `
+          <form class="widget-form" novalidate>
+              <div class="widget-control">
+                  <input name="input" id="input" class="input" type="text" placeholder="Some text to enter..." required>
+              </div>
+              <div>
+                  <button type="submit" class="btn">Click to toggle popover</button>
+              </div>
+          </form>    
+          `;
+    this.container.appendChild(form);
+  }
+
+  drawTooltip() {
+    const tooltipElement = document.createElement('div');
+
+    tooltipElement.classList.add('tooltip', 'hidden');
+
+    document.body.appendChild(tooltipElement);
+
+    const tooltipTitle = document.createElement('div');
+    tooltipTitle.classList.add('tooltip-title');
+    tooltipTitle.textContent = this.title;
+    tooltipElement.appendChild(tooltipTitle);
+
+    const tooltipMessage = document.createElement('div');
+    tooltipMessage.classList.add('tooltip-message');
+    tooltipMessage.textContent = this.message;
+    tooltipElement.appendChild(tooltipMessage);
+  }
+
+  showTooltip() {
+    const tooltipElement = document.querySelector('.tooltip');
+    tooltipElement.classList.remove('hidden');
+  }
+
+  closeTooltip() {
+    const tooltipElement = document.querySelector('.tooltip');
+    tooltipElement.classList.add('hidden');
   }
 
   bindToDOM() {
@@ -20,9 +62,12 @@ export default class Tooltip {
     const { target } = e;
 
     if (target.classList.contains('btn')) {
-      this.draw.showTooltip();
+      const { left, top } = target.getBoundingClientRect();
+      document.querySelector('.tooltip').style.left = `${left}px`;
+      document.querySelector('.tooltip').style.top = `${top - 80}px`;
+      this.showTooltip();
     } else {
-      this.draw.closeTooltip();
+      this.closeTooltip();
     }
   }
 }
